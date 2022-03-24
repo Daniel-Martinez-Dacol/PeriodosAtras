947083c837e4b75a90d93e33854e26a6ce478a61
Daniel <danieldaol99@gmail.com>
2022-03-24 17:31:37 -0300

    Add project files.

---
+++ PeriodosAtras.ConsoleApp/PeriodoAtras.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeriodosAtras.ConsoleApp
+{
+    internal class PeriodoAtras
+    {
+
+        public DateTime data;
+
+        public TimeSpan VerificarPeriodo()
+        {
+            TimeSpan intervalo = DateTime.Now - this.data;
+            int qtdDeDias, qtdDeSemanas, qtdDeMeses,qtdDeAnos;
+
+            qtdDeAnos = intervalo.Days / 365;
+            qtdDeMeses = qtdDeAnos / 28;
+            qtdDeSemanas =  qtdDeMeses % 7;
+            qtdDeDias = intervalo.Days;
+
+            if (intervalo.Days == 1)
+            {
+                Console.WriteLine($"{qtdDeDias} dia atrás");
+
+
+            }
+            else if (intervalo.Days > 1 && intervalo.Days < 7)
+            {
+                Console.WriteLine($"{qtdDeDias} dias atrás");
+            }
+            else if (intervalo.Days >= 7 && intervalo.Days < 28)
+            {
+                if (intervalo.Days >= 7 && intervalo.Days < 14)
+                {
+                    if (intervalo.Days == 7)
+                    {
+                        Console.WriteLine($"{qtdDeSemanas} semana atras");
+                    }
+                    Console.WriteLine($"{qtdDeSemanas} semanas e {qtdDeDias} dias atras");
+
+                }
+
+
+            }
+            //Mes:
+            if (intervalo.Days == 28)
+            {
+                Console.WriteLine($"{qtdDeMeses} Mes atras");
+            }
+            if (intervalo.Days > 28)
+            {
+                
+            }
+            //semana:
+
+            //dias: 
+
+            //horas
+
+            //minutos
+
+            //segundos:
+
+
+            return intervalo;
+
+
+
+
+
+        }
+    }
+}
---
+++ PeriodosAtras.ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PeriodosAtras.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PeriodoAtras periodo1 = new PeriodoAtras();
+
+            periodo1.data =Convert.ToDateTime("23/03/2022");
+            periodo1.VerificarPeriodo();
+
+            periodo1.data = Convert.ToDateTime("22/03/2022");
+            periodo1.VerificarPeriodo();
+
+            periodo1.data = Convert.ToDateTime("17/03/2022");
+            periodo1.VerificarPeriodo();
+
+            periodo1.data = Convert.ToDateTime("17/01/2022");
+            periodo1.VerificarPeriodo();
+
+            periodo1.data = Convert.ToDateTime("21/01/2022");
+            periodo1.VerificarPeriodo();
+
+            periodo1.data = Convert.ToDateTime("24/03/2011");
+            periodo1.VerificarPeriodo();
+
+            Console.ReadLine();
+
+        }
+    }
+}
